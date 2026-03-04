6e3b579434c2317f9fdeae2b59a85597922590b7
Peanutt42 <p3anutt42@gmail.com>
2026-03-04 20:24:21 +0100

    nvim: added tabs using barbar.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,6 +43,11 @@ require("lazy").setup({
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
+    opts = {
+      window = {
+        position = "right"
+      }
+    }
   },
   {
     "hrsh7th/nvim-cmp",
@@ -100,7 +105,20 @@ require("lazy").setup({
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {}
-  }
+  },
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+	  'nvim-tree/nvim-web-devicons',
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      insert_at_start = false,
+	  auto_hide = 1,
+    },
+    version = '^1.0.0',
+  },
 }, {
   ui = { border = "rounded" },
   checker = { enabled = false },
