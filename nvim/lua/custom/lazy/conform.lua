19b2ba09793b32cbf61669213c4bb93504f63e39
Peanutt42 <p3anutt42@gmail.com>
2026-03-20 02:12:11 +0100

    nvim: added Enable-/DisableAutoformat commands for conform

--- nvim/lua/custom/lazy/conform.lua
+++ nvim/lua/custom/lazy/conform.lua
@@ -16,6 +16,10 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(_)
+			if vim.g.disable_autoformat then
+				return
+			end
+
 			return {
 				timeout_ms = 500,
 				lsp_format = 'fallback',
@@ -25,4 +29,22 @@ return {
 			rust = { "rustfmt", lsp_format = "fallback" },
 		},
 	},
+	config = function(_, opts)
+		require("conform").setup(opts)
+
+		vim.api.nvim_create_user_command(
+			"DisableAutoformat",
+			function()
+				vim.g.disable_autoformat = true
+			end,
+			{ desc = "Disables auto formatting" }
+		)
+		vim.api.nvim_create_user_command(
+			"EnableAutoformat",
+			function()
+				vim.g.disable_autoformat = false
+			end,
+			{ desc = "Enables auto formatting" }
+		)
+	end,
 }
--- nvim/lua/custom/lazy/neotree.lua
+++ nvim/lua/custom/lazy/neotree.lua
@@ -8,41 +8,40 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	lazy = false, -- neo-tree will lazily load itself
-	config = function()
-		require("neo-tree").setup(
-		---@module 'neo-tree'
-		---@type neotree.Config
-			{
-				enable_cursor_hijack = true,
-				window = {
-					position = "right",
-					mappings = {
-						["<C-b>"] = "close_window",
-						["<leader>b"] = "close_window",
-						["<esc>"] = "close_window",
-						["<leftrelease>"] = "open",
-					},
-					mapping_options = {
-						noremap = true,
-						nowait = true,
-					}
-				},
-				buffers = {
-					follow_current_file = {
-						enabled = true,
-					},
-				},
-				default_component_configs = {
-					indent = {
-						padding = 0,
-						with_markers = true,
-						indent_marker = "│",
-						last_indent_marker = "└", -- "│",
-						highlight = "NeoTreeIndentMarker",
-					}
-				}
+	---@module 'neo-tree'
+	---@type neotree.Config
+	opts = {
+		enable_cursor_hijack = true,
+		window = {
+			position = "right",
+			mappings = {
+				["<C-b>"] = "close_window",
+				["<leader>b"] = "close_window",
+				["<esc>"] = "close_window",
+				["<leftrelease>"] = "open",
+			},
+			mapping_options = {
+				noremap = true,
+				nowait = true,
 			}
-		)
+		},
+		buffers = {
+			follow_current_file = {
+				enabled = true,
+			},
+		},
+		default_component_configs = {
+			indent = {
+				padding = 0,
+				with_markers = true,
+				indent_marker = "│",
+				last_indent_marker = "└", -- "│",
+				highlight = "NeoTreeIndentMarker",
+			}
+		}
+	},
+	config = function(_, opts)
+		require("neo-tree").setup(opts)
 
 		vim.api.nvim_set_hl(0, "NeoTreeIndentMarker", { fg = "#2F3337" })
 	end,
