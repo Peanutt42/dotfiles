1c5048288785c7e91bcf08aaa9b5d0da3a3b36f5
Peanutt42 <p3anutt42@gmail.com>
2026-04-12 17:36:36 +0200

    nvim: turned checkOnSave off for rust_analyzer as it mostly just dublicates macro errors
    this happens as both rust_analyzer and the cargo check/clippy report the
    same issue

--- nvim/lua/custom/servers/rust_analyzer.lua
+++ nvim/lua/custom/servers/rust_analyzer.lua
@@ -2,8 +2,13 @@
 return {
 	settings = {
 		["rust-analyzer"] = {
-			checkOnSave = true,
-			check = { command = 'clippy' },
+			-- we dont need `checkOnSave`, as rust-analyzer already gives us all the errors we need.
+			-- this option can dublicate macro errors, as both rust-analyzer and the `cargo check/clippy` report the macro error
+			checkOnSave = false,
+
+			-- if for some reason i someday need to have the accurate `cargo check/clippy` output, here it is
+			-- checkOnSave = true,
+			-- check = { command = 'clippy' },
 		}
 	}
 }
