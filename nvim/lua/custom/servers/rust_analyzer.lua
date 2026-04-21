5ce11710f736bad6ac3f7f64839ef9d6fc431825
Peanutt42 <p3anutt42@gmail.com>
2026-04-21 18:14:30 +0200

    nvim: added typst lsp

--- nvim/lua/custom/servers/rust_analyzer.lua
+++ nvim/lua/custom/servers/rust_analyzer.lua
@@ -2,13 +2,8 @@
 return {
 	settings = {
 		["rust-analyzer"] = {
-			-- we dont need `checkOnSave`, as rust-analyzer already gives us all the errors we need.
-			-- this option can dublicate macro errors, as both rust-analyzer and the `cargo check/clippy` report the macro error
-			checkOnSave = false,
-
-			-- if for some reason i someday need to have the accurate `cargo check/clippy` output, here it is
-			-- checkOnSave = true,
-			-- check = { command = 'clippy' },
+			checkOnSave = true,
+			check = { command = 'clippy' },
 		}
 	}
 }
