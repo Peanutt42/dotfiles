7ad1051b2cc9ded47bfc70494205d3e7159139c2
Peanutt42 <p3anutt42@gmail.com>
2026-05-07 02:29:46 +0200

    nvim: made typst-preview open in chrome
    this is better since by default, it opens in my zen browser window,
    which is not practical for previewing. i dont use chrome for anything
    other than previewing typst so this is much better

--- nvim/lua/custom/lazy/typst-preview.lua
+++ nvim/lua/custom/lazy/typst-preview.lua
@@ -2,5 +2,8 @@ return {
 	'chomosuke/typst-preview.nvim',
 	lazy = false,
 	version = '1.*',
-	opts = {},
+	opts = {
+		-- chrome, because i dont use chrome for anything else -> doesnt hijack my zen browser window
+		open_cmd = 'com.google.Chrome %s'
+	},
 }
