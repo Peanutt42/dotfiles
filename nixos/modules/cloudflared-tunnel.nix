8b16b71d1a943163ea5c1f81c6ee5eecce85f339
Peanutt42 <p3anutt42@gmail.com>
2026-05-20 22:11:59 +0200

    nixos(pi): added proper minimal config.yml for cloudflared tunnel

--- nixos/modules/cloudflared-tunnel.nix
+++ nixos/modules/cloudflared-tunnel.nix
@@ -5,16 +5,22 @@
     cloudflared
   ];
 
+  environment.etc."cloudflared/config.yml".text = ''
+    tunnel: raspberrypi
+    credentials-file: /etc/cloudflared/4ca9765d-1875-4d76-bf02-7e4c88257fbe.json
+  '';
+
   systemd.services.cloudflared = {
     description = "Cloudflare Tunnel";
+
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
     wantedBy = [ "multi-user.target" ];
+
     serviceConfig = {
-      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel run raspberrypi";
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --config /etc/cloudflared/config.yml run";
       Restart = "on-failure";
       RestartSec = 5;
-      User = "peter";
     };
   };
 }
