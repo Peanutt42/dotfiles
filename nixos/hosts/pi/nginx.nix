7de6c929aa3755b362939c5cad103697f8480480
Peanutt42 <p3anutt42@gmail.com>
2026-06-16 08:27:45 +0200

    nixos (pi): added raumreservierung.peternhennig.de to nginx

--- nixos/hosts/pi/nginx.nix
+++ nixos/hosts/pi/nginx.nix
@@ -5,6 +5,21 @@
     enable = true;
     virtualHosts."peternhennig.de" = {
       root = "/var/www/portfolio";
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 8081;
+        }
+      ];
+    };
+    virtualHosts."raumreservierung.peternhennig.de" = {
+      root = "/var/www/raumreservierung";
+      listen = [
+        {
+          addr = "127.0.0.1";
+          port = 8082;
+        }
+      ];
     };
   };
 }
