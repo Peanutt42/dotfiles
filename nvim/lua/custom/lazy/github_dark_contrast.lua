994c651b456744e36682b19173d7cc45f54f76d6
Peanutt42 <p3anutt42@gmail.com>
2026-08-14 15:20:55 +0200

    nvim: made text buffers transparent on transparent terminals

--- nvim/lua/custom/lazy/github_dark_contrast.lua
+++ nvim/lua/custom/lazy/github_dark_contrast.lua
@@ -5,5 +5,12 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd("colorscheme github_dark_contrast")
+
+		-- make text buffers transparent when the terminal itself is also transparent
+		vim.cmd([[
+			highlight Normal guibg=none ctermbg=none
+			highlight NormalNC guibg=none ctermbg=none
+			highlight NonText guibg=none ctermbg=none
+		]])
 	end,
 }
