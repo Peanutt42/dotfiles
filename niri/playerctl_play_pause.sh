d6adc20aba906e1cb1a8029d6ab18e33f09b1509
Peanutt42 <p3anutt42@gmail.com>
2026-08-31 22:24:04 +0200

    niri: made playerctl stop all players and only play one player if no are playing
    the added logic is a in small bash script

---
+++ niri/playerctl_play_pause.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Pauses all players if any is playing and only starts one player if no player is playing
+#
+# needed because a simple "playerctl --all-players play-pause" can play all players
+# and a simple "playerctl play-pause" may only pause one player eventhough multiple are playing
+
+if playerctl --all-players status 2>/dev/null | grep -q '^Playing$'; then
+    playerctl --all-players pause
+else
+    playerctl --player=spotify play
+fi
